711ea57b66d4501978d3f7a12e5286dce760316f
Daniela Farias <daniela.farias@aurum.com.br>
2024-06-19 02:18:31 -0300

    Inferência: Tipos não declaráveis

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import React from "react"
+import Button from "./Button";
+
 function App() {
+  const [total, setTotal] = React.useState(0);
+
+  function increment() {
+    setTotal((total) => total + 1);
+  }
+
   return (
     <div>
-      React
+      <p>Total: {total}</p>
+      <button onClick={increment}>Incrementar</button>
+      <Button />
     </div>
   )
 }
