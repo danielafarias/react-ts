245268654861155230800d940d400a5d34cb486d
Daniela Farias <daniela.farias@aurum.com.br>
2024-06-30 20:23:29 -0300

    Correção do exercício 4 - vídeo

--- src/Video.tsx
+++ src/Video.tsx
@@ -3,53 +3,49 @@ import videoSrc from './video.mp4';
 
 function Video() {
   const [isPlaying, setIsPlaying] = React.useState(false);
-  const [isMuted, setIsMuted] = React.useState(false);
   const video = React.useRef<HTMLVideoElement>(null);
 
   const handlePlay = () => {
     video.current?.play();
-    setIsPlaying(true);
   }
 
   const handlePause = () => {
     video.current?.pause();
-    setIsPlaying(false);
   }
 
   const handleForward = () => {
-    if (video.current) {
-        video.current.currentTime = video.current.currentTime +2;
-    }
+    if (video.current) video.current.currentTime += 2;
   }
 
-  const handlePlaybackRate = () => {
-    if (video.current) {
-        const newPlaybackRate = video.current.playbackRate < 2 ? video.current.playbackRate + 0.5 : 0.5;
-        video.current.playbackRate = newPlaybackRate;
-    }
+  const handlePlaybackRate = (speed: number) => {
+    if (video.current) video.current.playbackRate = speed;
   }
 
-  const handlePictureInPicture = () => {
-    if (video.current) {
-        if (document.pictureInPictureElement) {
-          document.exitPictureInPicture();
-        } else if (document.pictureInPictureEnabled) {
-          video.current.requestPictureInPicture();
-        }
+  const handlePictureInPicture = async () => {
+    if (!video.current) return;
+    if (document.pictureInPictureElement) {
+        await document.exitPictureInPicture();
+    } else {
+        await video.current.requestPictureInPicture();
     }
+    
   }
 
   const handleMute = () => {
-    setIsMuted(!isMuted);
+    if (video.current) video.current.muted = !video.current.muted;
   }
 
   return (
     <div>
       <div className="flex">
-        <button onClick={handlePlay}>Play</button>
-        <button onClick={handlePause}>Pause</button>
+        {
+            !isPlaying ? <button onClick={handlePlay}>Play</button> 
+            : <button onClick={handlePause}>Pause</button>
+        }
         <button onClick={handleForward}>{">>"}</button>
-        <button onClick={handlePlaybackRate}>x1</button>
+        <button onClick={() => handlePlaybackRate(0.5)}>x0.5</button>
+        <button onClick={() => handlePlaybackRate(1)}>x1</button>
+        <button onClick={() => handlePlaybackRate(2)}>x2</button>
         <button onClick={handlePictureInPicture}>Picture mode</button>
         <button onClick={handleMute}>Mute</button>
       </div>
@@ -57,8 +53,7 @@ function Video() {
       <video 
         controls 
         ref={video} 
-        src={videoSrc} 
-        muted={isMuted}
+        src={videoSrc}
         onPlay={() => setIsPlaying(true)} 
         onPause={() => setIsPlaying(false)}
       ></video>
